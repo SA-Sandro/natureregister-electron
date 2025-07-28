cebacaf340c6762912451d3fcb6884aa2ccc67f8
Sandro.dev <sandrosuarezaranda@gmail.com>
2025-07-28 23:51:38 +0200

    Improve DDD structure

--- server/application/ImagesManagementService.ts
+++ server/application/ImagesManagementService.ts
@@ -1,28 +1,10 @@
-import fs from 'fs'
-import path from 'path'
 import { Image } from '../domain/entities/Image'
+import { ImageRepository } from '../domain/repositories/ImageRepository'
 
 export class ImagesManagementService {
-  private images: Image[] = []
-
-  constructor(private readonly VALID_EXT: string[] = ['.jpg', '.jpeg', '.png', '.webp']) {}
+  constructor(private readonly imageRepository: ImageRepository) {}
 
   public async getAllImagesFromSelectedFolder(folderPath: string): Promise<Image[]> {
-    const files = fs.readdirSync(folderPath)
-    const imageFiles = files.filter((file) =>
-      this.VALID_EXT.includes(path.extname(file).toLowerCase()),
-    )
-
-    this.images = this.toImage(imageFiles, folderPath)
-    return this.images
-  }
-
-  private toImage(files: string[], folderPath: string): Image[] {
-    return files.map((file) => {
-      const filePath = path.join(folderPath, file)
-      const stat = fs.statSync(filePath)
-
-      return new Image(filePath, stat.size, path.extname(file).toLowerCase(), stat.birthtime)
-    })
+    return this.imageRepository.getImagesFromFolder(folderPath)
   }
 }
