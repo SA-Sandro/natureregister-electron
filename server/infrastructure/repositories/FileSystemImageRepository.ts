4474b296e18644237ec3ae0f1819bdb42c0ab722
cjag99 <cjag99@gmail.com>
2025-07-29 22:18:01 +0200

    Fix bug that prevents from retrieving all images on selected path

--- server/infrastructure/repositories/FileSystemImageRepository.ts
+++ server/infrastructure/repositories/FileSystemImageRepository.ts
@@ -1,20 +1,45 @@
-import fs from 'fs'
+import fs from 'fs/promises'
 import path from 'path'
 import { Image } from '../../domain/entities/Image'
 import { ImageRepository } from '../../domain/repositories/ImageRepository'
 
 export class FileSystemImageRepository implements ImageRepository {
-  constructor(private readonly VALID_EXT: string[] = ['.jpg', '.jpeg', '.png', '.webp']) {}
+  private readonly VALID_EXT: string[]
+
+  constructor(validExt: string[] = ['.jpg', '.jpeg', '.png', '.webp']) {
+    this.VALID_EXT = validExt
+  }
 
   public async getImagesFromFolder(folderPath: string): Promise<Image[]> {
-    const files = fs.readdirSync(folderPath)
-    const imageFiles = files.filter((file) =>
-      this.VALID_EXT.includes(path.extname(file).toLowerCase()),
-    )
-    return imageFiles.map((file) => {
+    const allImages: Image[] = []
+    let files: string[]
+
+    try {
+      files = await fs.readdir(folderPath)
+    } catch (err) {
+      console.error('Error al leer la carpeta:', err)
+      return allImages
+    }
+
+    for (const file of files) {
       const filePath = path.join(folderPath, file)
-      const stat = fs.statSync(filePath)
-      return new Image(filePath, stat.size, path.extname(file).toLowerCase(), stat.birthtime)
-    })
+      let fileStat
+      try {
+        fileStat = await fs.stat(filePath)
+      } catch (error) {
+        console.error('Error al obtener la ruta: ', error)
+        continue
+      }
+
+      if (fileStat.isDirectory()) {
+        const nestedImages = await this.getImagesFromFolder(filePath) // Await the recursion
+        allImages.push(...nestedImages)
+      } else if (this.VALID_EXT.includes(path.extname(file).toLowerCase())) {
+        allImages.push(
+          new Image(filePath, fileStat.size, path.extname(file).toLowerCase(), fileStat.birthtime),
+        )
+      }
+    }
+    return allImages
   }
 }
