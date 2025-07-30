ef2dc1378f2902bf05d20631de2e3fec01fad1e1
cjag99 <cjag99@gmail.com>
2025-07-30 18:55:54 +0200

    Implement suggested changes

--- server/infrastructure/repositories/FileSystemImageRepository.ts
+++ server/infrastructure/repositories/FileSystemImageRepository.ts
@@ -1,15 +1,19 @@
-import fs from 'fs/promises'
+import { promises as fs, Stats } from 'fs'
 import path from 'path'
 import { Image } from '../../domain/entities/Image'
 import { ImageRepository } from '../../domain/repositories/ImageRepository'
 
 export class FileSystemImageRepository implements ImageRepository {
-  private readonly VALID_EXT: string[]
+  private readonly VALID_EXT: string[] = ['.jpg', '.jpeg', '.png', '.webp']
 
-  constructor(validExt: string[] = ['.jpg', '.jpeg', '.png', '.webp']) {
-    this.VALID_EXT = validExt
+  private async getFileStats(folderPath: string): Promise<Stats | null> {
+    try {
+      return await fs.stat(folderPath)
+    } catch (error) {
+      console.error('An error ocurred while accesing the path:', error)
+      return null
+    }
   }
-
   public async getImagesFromFolder(folderPath: string): Promise<Image[]> {
     const allImages: Image[] = []
     let files: string[]
@@ -17,19 +21,14 @@ export class FileSystemImageRepository implements ImageRepository {
     try {
       files = await fs.readdir(folderPath)
     } catch (err) {
-      console.error('Error al leer la carpeta:', err)
+      console.error('An error ocurred while reading the folder:', err)
       return allImages
     }
 
     for (const file of files) {
       const filePath = path.join(folderPath, file)
-      let fileStat
-      try {
-        fileStat = await fs.stat(filePath)
-      } catch (error) {
-        console.error('Error al obtener la ruta: ', error)
-        continue
-      }
+      const fileStat = await this.getFileStats(filePath)
+      if (!fileStat) continue
 
       if (fileStat.isDirectory()) {
         const nestedImages = await this.getImagesFromFolder(filePath) // Await the recursion
