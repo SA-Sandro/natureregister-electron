43de9125f3831f2dd0553a2e9caad5b234d7c095
Sandro.dev <sandrosuarezaranda@gmail.com>
2025-08-18 22:59:49 +0200

    Add InvalidImageExtension and ImageExtension classes for better image validation

---
+++ server/domain/exceptions/InvalidImageExtension.ts
@@ -0,0 +1,6 @@
+export class InvalidImageExtension extends Error {
+  constructor(extension: string) {
+    super(`Invalid image extension: ${extension}.`);
+    this.name = 'InvalidImageExtension';
+  }
+}
