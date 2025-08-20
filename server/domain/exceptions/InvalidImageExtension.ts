84536bb680fdcf5ad00663c59d6ab63f72f65beb
Sandro.dev <sandrosuarezaranda@gmail.com>
2025-08-20 23:50:24 +0200

    Fix error message formatting in InvalidImageExtension class

--- server/domain/exceptions/InvalidImageExtension.ts
+++ server/domain/exceptions/InvalidImageExtension.ts
@@ -1,6 +1,6 @@
 export class InvalidImageExtension extends Error {
   constructor(extension: string) {
-    super(`Invalid image extension: ${extension}.`);
+    super(`Invalid image extension: ${extension}`);
     this.name = 'InvalidImageExtension';
   }
 }
