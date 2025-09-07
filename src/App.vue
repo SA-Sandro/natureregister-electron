94796f3b13347695c39cce6f2413c238372242ee
Sandro.dev <sandrosuarezaranda@gmail.com>
2025-09-07 23:01:23 +0200

    Create MainPage layou which implement a header to the App

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,12 @@
 <script setup lang="ts">
-import SelectFolder from '@/components/SelectFolder.vue';
 import ImageLibrary from '@/components/ImageLibrary.vue';
+import PageLayout from './layouts/PageLayout.vue';
 </script>
 
 <template>
-  <div>
-    <div>
-      <img alt="Vue logo" src="/logo_nt.png" width="125" />
-    </div>
-    <h1>Imágenes</h1>
-    <p>Revisa la consola para ver las imágenes obtenidas.</p>
-    <SelectFolder />
+  <PageLayout>
     <ImageLibrary />
-  </div>
+  </PageLayout>
 </template>
 
 <style scoped></style>
---
+++ src/components/MainHeader.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import SelectFolder from './SelectFolder.vue';
+</script>
+
+<template>
+  <div class="w-full h-25 flex justify-between items-center p-3">
+    <div>
+      <div class="flex items-center">
+        <img src="/logo_nt.png" alt="Nature Register Logo" class="w-25" />
+        <span class="ml-4 text-2xl font-semibold text-green-700 tracking-wide">
+          Nature Register
+        </span>
+      </div>
+    </div>
+    <div>
+      <SelectFolder />
+    </div>
+  </div>
+</template>
