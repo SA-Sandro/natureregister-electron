e308f7a2efa984062aa99a99562f5fdfd1926d7e
Sandro.dev <sandrosuarezaranda@gmail.com>
2025-10-18 18:05:36 +0200

    Refactor getRecordsFromSQLITE and seed scripts to improve data handling and add local image renaming functionality

--- scripts/getAllRecordsFromSQLITE.ts
+++ scripts/getAllRecordsFromSQLITE.ts
@@ -1,53 +1,56 @@
 import Database from 'better-sqlite3';
 import { randomUUID } from 'node:crypto';
 import { SpecimenObservation } from '../server/domain/entities/SpecimenObservation';
+import { env } from 'node:process';
 
 const getGenus = (scientificName: string) => {
-  const genus = scientificName.trim().split(' ')[0];
-  return genus;
+  if (scientificName.trim().toLowerCase() === 'sin determinar') return 'Sin determinar';
+  return scientificName.trim().split(' ')[0];
 };
 
 type ObservationData = {
-  observations: SpecimenObservation[];
+  observation: SpecimenObservation[];
   imageUrl: string;
 };
 
 export const getRecordsFromSQLITE = (): ObservationData[] => {
-  const db = new Database(
-    'C:/Users/The_9/Documents/Nature Register/Ejecutable/observacionesBD.sqlite',
-  );
+  const db = new Database(env.SAMPLE_DATABASE);
 
-  const observation: ObservationData[] = [];
+  const observations: ObservationData[] = [];
 
-  const rows = db.prepare('SELECT * FROM REGISTRO').all();
+  try {
+    const rows = db.prepare('SELECT * FROM aranea_sample').all();
+    rows.forEach((row: any) => {
+      const uuid = randomUUID();
+      const scientificName = row.nombreCientifico || 'Unknown';
+      const genus = getGenus(scientificName);
+      const family = row.familia || 'Unknown';
+      const orden = row.orden || 'Unknown';
+      const date = row.fecha || 'Unknown';
+      const coordinates = row.coordenadas || '0,0';
+      const observationSite = row.lugarObservacion || 'Unknown';
+      const locality = '';
+      const province = row.provincia || 'Unknown';
+      const comments = '';
+      const imageUrl = row.URLImagen || '';
 
-  rows.forEach((row: any) => {
-    const uuid = randomUUID();
-    const scientificName = row.nombreCientifico || 'Unknown';
-    const genus = getGenus(scientificName);
-    const family = row.familia || 'Unknown';
-    const orden = row.orden || 'Unknown';
-    const date = row.fecha || 'Unknown';
-    const coordinates = row.coordenadas || '0,0';
-    const municipality = row.localidad || 'Unknown';
-    const locality = '';
-    const province = row.provincia || 'Unknown';
-    const comments = '';
-    const imageUrl = row.URLImagen || '';
+      const specimenObservation = new SpecimenObservation(
+        uuid,
+        { scientificName, genus, family, orden },
+        date,
+        { coordinates, observationSite, province, locality },
+        comments,
+      );
 
-    const specimenObservation = new SpecimenObservation(
-      uuid,
-      { scientificName, genus, family, orden },
-      date,
-      { coordinates, municipality, province, locality },
-      comments,
-    );
-
-    observation.push({
-      observations: [specimenObservation],
-      imageUrl,
+      observations.push({
+        observation: [specimenObservation],
+        imageUrl,
+      });
     });
-  });
 
-  return observation;
+    return observations;
+  } catch (error) {
+    console.error('Error al leer la base de datos SQLITE:', error);
+    throw error;
+  }
 };
--- scripts/seed.ts
+++ scripts/seed.ts
@@ -1,3 +1,5 @@
+import fs from 'fs/promises';
+
 import { PrismaClient } from '@prisma/client';
 import { PrismaSpecimenObservationRepository } from '../server/infrastructure/repositories/PrismaSpecimenObservationRepository';
 
@@ -6,6 +8,21 @@ import { SpecimenInfo } from '../server/domain/valueObjects/SpecimenInfo';
 import { GeospatialData } from '../server/domain/valueObjects/GeospatialData';
 import { getRecordsFromSQLITE } from './getAllRecordsFromSQLITE';
 
+const renameLocalImage = async (currentPath: string, uuid: string): Promise<void> => {
+  console.log(`Renombrando imagen: ${currentPath}`);
+  const splittedPath = currentPath.split(/[/\\]/);
+  splittedPath[splittedPath.length - 1] =
+    `${uuid}${splittedPath[splittedPath.length - 1].slice(splittedPath[splittedPath.length - 1].lastIndexOf('.'))}`;
+  const newPath = splittedPath.join('/');
+
+  if (currentPath === newPath) {
+    console.log(`⚠️ La imagen ya tiene el nombre correcto: ${currentPath}`);
+    return;
+  }
+  await fs.rename(currentPath, newPath);
+  console.log(`✅ Imagen renombrada a ${newPath}`);
+};
+
 async function main() {
   const prisma = new PrismaClient();
   const repo = new PrismaSpecimenObservationRepository(prisma);
@@ -13,7 +30,7 @@ async function main() {
   const observations = getRecordsFromSQLITE();
 
   observations.forEach(async (data) => {
-    for (const obs of data.observations) {
+    for (const obs of data.observation) {
       const specimenInfoVO = obs.getSpecimenInfo();
       const geoVO = obs.getGeospatialData();
       const specimenInfoRow = await prisma.specimenInfo.create({
@@ -21,14 +38,15 @@ async function main() {
           scientificName: specimenInfoVO.scientificName,
           genus: specimenInfoVO.genus,
           family: specimenInfoVO.family,
+          orden: specimenInfoVO.orden,
         },
       });
       const geoRow = await prisma.geoSpatialData.create({
         data: {
           coordinates: geoVO.coordinates,
-          municipality: geoVO.municipality,
-          province: geoVO.province,
           locality: geoVO.locality,
+          province: geoVO.province,
+          observationSite: geoVO.observationSite,
         },
       });
       const specimenInfoWithId = new SpecimenInfo(
@@ -36,6 +54,7 @@ async function main() {
         specimenInfoVO.scientificName,
         specimenInfoVO.genus,
         specimenInfoVO.family,
+        specimenInfoVO.orden,
       );
       const geoWithId = new GeospatialData(
         geoRow.id,
@@ -51,8 +70,13 @@ async function main() {
         geoWithId,
         obs.getComments(),
       );
-      await repo.save(observationWithIds);
-      console.log(`✅ Observación ${observationWithIds.getUuid()} guardada con éxito`);
+      try {
+        await renameLocalImage(data.imageUrl, obs.getUuid());
+        await repo.save(observationWithIds);
+        console.log(`✅ Observación ${observationWithIds.getUuid()} guardada con éxito`);
+      } catch (error) {
+        console.error(`❌ Error guardando la observación ${observationWithIds.getUuid()}:`, error);
+      }
     }
   });
 }
