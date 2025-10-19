a0c721184bf0b256f10afa909f12574874ea4fab
Sandro.dev <sandrosuarezaranda@gmail.com>
2025-10-19 11:22:12 +0200

    Add new endpoint to get all specimenObservation and add a middleware to handle all errors from api

--- server/infrastructure/server.ts
+++ server/infrastructure/server.ts
@@ -4,6 +4,12 @@ import { ImagesRoutes } from '@infrastructure/routes/ImagesRoutes';
 import { ImagesController } from '@infrastructure/controllers/ImagesController';
 import { ImagesManagementService } from '@application/ImagesManagementService';
 import { FileSystemImageRepository } from '@infrastructure/repositories/FileSystemImageRepository';
+import { SpecimenObservationRoutes } from '@infrastructure/routes/SpecimenObservationRoutes';
+import { SpecimenObservationController } from '@infrastructure/controllers/SpecimenObservationController';
+import { SpecimenObservationManagementService } from '@application/SpecimenObservationManagementService';
+import { PrismaSpecimenObservationRepository } from '@infrastructure/repositories/PrismaSpecimenObservationRepository';
+import { PrismaClient } from '@prisma/client';
+import { errorHandlingMiddleware } from '@infrastructure/middleware';
 
 const app = express();
 
@@ -20,8 +26,18 @@ const imagesService = new ImagesManagementService(imageRepository);
 const imagesController = new ImagesController(imagesService);
 const imagesRoutes = new ImagesRoutes(imagesController);
 
-app.use('/api/images', imagesRoutes.router);
+const specimenObservationRepository = new PrismaSpecimenObservationRepository(new PrismaClient());
+const specimenObservationManagementService = new SpecimenObservationManagementService(
+  specimenObservationRepository,
+);
+const specimenObservationController = new SpecimenObservationController(
+  specimenObservationManagementService,
+);
+const specimenObservationRoutes = new SpecimenObservationRoutes(specimenObservationController);
 
+app.use('/api/images', imagesRoutes.router);
+app.use('/api/specimenObservations', specimenObservationRoutes.router);
 app.listen(3000, () => {
   console.log('Server is running on port 3000:  http://localhost:3000');
 });
+app.use(errorHandlingMiddleware);
