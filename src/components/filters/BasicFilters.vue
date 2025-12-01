ee3011eab87107d489661b152843211cb9049dea
Sandro.dev <sandrosuarezaranda@gmail.com>
2025-12-01 21:13:47 +0100

    Add ObservationsFilter and BasicFilters components for filtering by scientific name and locality

---
+++ src/components/filters/BasicFilters.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import LocalityFilter from '@/components/filters/LocalityFilter.vue';
+import ScientificNameFilter from '@/components/filters/ScientificNameFilter.vue';
+import { useImageStore } from '@/stores/imageStore';
+
+const filterByCriteria = () => {
+  const imageStore = useImageStore();
+  const selectedFilters = {
+    scientificNameValue: document.getElementById('sicientific-name-filter') as HTMLInputElement,
+    localityValue: document.getElementById('locality-filter') as HTMLInputElement,
+  };
+
+  imageStore.filterByScientificNameAndLocality(
+    selectedFilters.scientificNameValue.value,
+    selectedFilters.localityValue.value,
+  );
+};
+</script>
+
+<template>
+  <div class="flex justify-center items-center gap-6">
+    <div class="flex justify-center items-center gap-4">
+      <ScientificNameFilter />
+      <LocalityFilter />
+    </div>
+
+    <button
+      @click="filterByCriteria"
+      class="bg-green-500 cursor-pointer text-white px-4 py-2 rounded-lg shadow hover:bg-green-700 transition-all"
+    >
+      Buscar
+    </button>
+  </div>
+</template>
